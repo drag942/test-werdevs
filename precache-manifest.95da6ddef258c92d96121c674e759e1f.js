c5be4769513f2fd8a724f2ce3443bc8f564fa95d
Dima <infermo94.09@gmail.com>
2020-09-26 01:47:53 +0300

    Updates

---
+++ precache-manifest.95da6ddef258c92d96121c674e759e1f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dda0acae123c79209f9b07c5df09a494",
+    "url": "/test-werdevs/index.html"
+  },
+  {
+    "revision": "a0e52b963e9d87cc4a24",
+    "url": "/test-werdevs/static/css/main.ac32ffcf.chunk.css"
+  },
+  {
+    "revision": "bae1f7db519f8760852e",
+    "url": "/test-werdevs/static/js/2.63966d1e.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/test-werdevs/static/js/2.63966d1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0e52b963e9d87cc4a24",
+    "url": "/test-werdevs/static/js/main.4d3cf7a7.chunk.js"
+  },
+  {
+    "revision": "e7269de7513a4377362b",
+    "url": "/test-werdevs/static/js/runtime-main.2246af03.js"
+  },
+  {
+    "revision": "37a75b54218416da18cdf1fa0650c569",
+    "url": "/test-werdevs/static/media/artboard.37a75b54.jpg"
+  },
+  {
+    "revision": "b1a38376a7a592cc41e7b6701aada3d1",
+    "url": "/test-werdevs/static/media/logo.b1a38376.svg"
+  }
+]);
