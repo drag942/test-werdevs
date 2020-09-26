227b87cd07d8f9c68448b314a56f3fb3e9b05fd1
Dima <infermo94.09@gmail.com>
2020-09-26 09:43:31 +0300

    -add links in mobile navigation

--- src/components/Header/MobileNavBar/MobileNavBar.tsx
+++ src/components/Header/MobileNavBar/MobileNavBar.tsx
@@ -6,6 +6,7 @@ import styles from './MobileNavBar.module.css'
 
 import MenuIcon from '@material-ui/icons/Menu';
 import {Link , Divider, ListItem, List, IconButton, SwipeableDrawer} from "@material-ui/core";
+import {NavLink} from "react-router-dom";
 
 
 const MobileNavBar = () =>  {
@@ -22,28 +23,29 @@ const MobileNavBar = () =>  {
 
     return (
         <div className={styles.container}>
-            <IconButton  onClick={toggleDrawer(true)}><MenuIcon color={'primary'}/></IconButton>
+            <IconButton style={{marginRight: '10px'}}  onClick={toggleDrawer(true)}><MenuIcon/></IconButton>
             <SwipeableDrawer anchor={'left'} open={open} onClose={toggleDrawer(false)} onOpen={toggleDrawer(true)}  >
                 <List  onKeyDown={toggleDrawer(false)} onClick={toggleDrawer(false)}>
                     <ListItem><img className={styles.logo} alt={'logo'} src={logo}/></ListItem>
                     <Divider/>
                     <div>
                          <ListItem>
-                                <Link
-
-                                    color='inherit'
-                                >
-                                    {'Home'}
-                                </Link>
-                            </ListItem>
-                            <ListItem>
-                                <Link
-
-                                    color='inherit'
-                                >
-                                    {'About Us'}
-                                </Link>
-                            </ListItem>
+                             <NavLink
+                                 to={'/home'}
+                                 activeClassName={styles.active}
+                                 className={styles.link}
+                             >
+                                 Home
+                             </NavLink>
+                         </ListItem>
+                        <ListItem>
+                            <NavLink
+                                activeClassName={styles.active}
+                                className={styles.link}
+                                to={'/about'}>
+                                    About Us
+                            </NavLink>
+                        </ListItem>
                     </div>
 
                 </List>
